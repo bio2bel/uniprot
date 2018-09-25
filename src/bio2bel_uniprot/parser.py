38f63f3937d205a0d36fe7beb05632aaadcba544
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-09-25 14:58:34 +0200

    Pre-caching results

--- src/bio2bel_uniprot/parser.py
+++ src/bio2bel_uniprot/parser.py
@@ -2,8 +2,13 @@
 
 """Downloading and parsing functions for Bio2BEL UniProt."""
 
+import os
+import pickle
+
+import pandas as pd
+
 from bio2bel.downloading import make_df_getter
-from .constants import MAPPINGS_PATH, MAPPINGS_URL
+from .constants import MAPPINGS_PATH, MAPPINGS_URL, SLIM_MAPPINGS_PATH
 
 get_mappings_df = make_df_getter(MAPPINGS_URL, MAPPINGS_PATH, sep='\t')
 """Returns a file with the following columns:
@@ -32,4 +37,16 @@ get_mappings_df = make_df_getter(MAPPINGS_URL, MAPPINGS_PATH, sep='\t')
 """
 
 SLIM_COLUMNS = ['UniProtKB-AC', 'UniProtKB-ID', 'GeneID', 'NCBI-Taxon']
-get_slim_mappings_df = make_df_getter(MAPPINGS_URL, MAPPINGS_PATH, sep='\t', usecols=[0, 1, 2, 12], names=SLIM_COLUMNS)
+_get_slim_mappings_df = make_df_getter(MAPPINGS_URL, MAPPINGS_PATH, sep='\t', usecols=[0, 1, 2, 12], names=SLIM_COLUMNS)
+
+
+def get_slim_mappings_df() -> pd.DataFrame:
+    if os.path.exists(SLIM_MAPPINGS_PATH):
+        with open(SLIM_MAPPINGS_PATH, 'rb') as f:
+            return pickle.load(f)
+
+    df = _get_slim_mappings_df()
+    with open(SLIM_MAPPINGS_PATH, 'wb') as f:
+        pickle.dump(df, f)
+
+    return df
