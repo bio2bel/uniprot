ffcb3064df2f7ccd7842308742a3bdc2bcde6eae
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-09-26 12:01:59 +0200

    Add testing

--- src/bio2bel_uniprot/parser.py
+++ src/bio2bel_uniprot/parser.py
@@ -4,13 +4,14 @@
 
 import os
 import pickle
+from typing import Optional
 
 import pandas as pd
 
 from bio2bel.downloading import make_df_getter
 from .constants import MAPPINGS_PATH, MAPPINGS_URL, SLIM_MAPPINGS_PATH
 
-get_mappings_df = make_df_getter(MAPPINGS_URL, MAPPINGS_PATH, sep='\t')
+get_mappings_df = make_df_getter(MAPPINGS_URL, MAPPINGS_PATH, sep='\t', header=None)
 """Returns a file with the following columns:
 1. UniProtKB-AC
 2. UniProtKB-ID
@@ -40,13 +41,13 @@ SLIM_COLUMNS = ['UniProtKB-AC', 'UniProtKB-ID', 'GeneID', 'NCBI-Taxon']
 _get_slim_mappings_df = make_df_getter(MAPPINGS_URL, MAPPINGS_PATH, sep='\t', usecols=[0, 1, 2, 12], names=SLIM_COLUMNS)
 
 
-def get_slim_mappings_df() -> pd.DataFrame:
+def get_slim_mappings_df(url: Optional[str] = None) -> pd.DataFrame:
     """Get mappings between UniProt accession, identifier, Entrez gene, and NCBI taxonomny."""
     if os.path.exists(SLIM_MAPPINGS_PATH):
         with open(SLIM_MAPPINGS_PATH, 'rb') as f:
             return pickle.load(f)
 
-    df = _get_slim_mappings_df()
+    df = _get_slim_mappings_df(url=url)
     with open(SLIM_MAPPINGS_PATH, 'wb') as f:
         pickle.dump(df, f)
 
---
+++ tests/test_get.py
@@ -0,0 +1,15 @@
+import os
+import unittest
+
+from bio2bel_uniprot import get_mappings_df
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+URL = os.path.join(HERE, 'test.tsv')
+
+
+class TestGet(unittest.TestCase):
+    """Test getting data."""
+
+    def test_get_mappings(self):
+        df = get_mappings_df(url=URL)
+        self.assertEqual(6, len(df.index))
